259dd2977b2be1869e861ad61a78f22b8f761450
rhit-hahmcm <87050271+rhit-hahmcm@users.noreply.github.com>
2022-01-19 19:51:57 -0500

    Started working clothing edit page

--- RandomOutfitGenerator/app/src/main/java/edu/rosehulman/randomoutfitgenerator/ui/ClothingEditFragment.kt
+++ RandomOutfitGenerator/app/src/main/java/edu/rosehulman/randomoutfitgenerator/ui/ClothingEditFragment.kt
@@ -1,4 +1,23 @@
 package edu.rosehulman.randomoutfitgenerator.ui
 
-class ClothingEditFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import edu.rosehulman.randomoutfitgenerator.databinding.FragmentClothingEditBinding
+
+class ClothingEditFragment: Fragment() {
+    private lateinit var binding: FragmentClothingEditBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentClothingEditBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
 }
